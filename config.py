221566f2841b006eaad59f73c515c81f4d8da7a0
tungtungtun <enginemaker31@gmail.com>
2025-07-06 14:47:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,15 +26,15 @@ admins = {}
 OLD_PMS = {}
 AUDIO_CALL = {}
 VIDEO_CALL = {}
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-SESSION_STRING = getenv("SESSION_STRING", "")
+API_ID = int(getenv("API_ID", "25852187"))
+API_HASH = getenv("API_HASH", "817bd34c0139d5819e79051da8a2a82f")
+BOT_TOKEN = getenv("BOT_TOKEN", "7641782248:AAEqjuCghZTdFoYQECUrCI_ueAgaSwEWhJ8")
+SESSION_STRING = getenv("SESSION_STRING", "BQGKeRsAseEddmE0D7jB9nO__36JS-NxEd70jm9GDHp8a4ZkL4o_U-AX8lKLdsfnTyp3a_5q3wW-oWKS_qJOxkN_Qpt293wvzLjRs_Rjrz4UYOR9Bx6kOvZpIYTHgyKr03my3sQLlnfKcOmEqtO_OTs5VRqvKoM8t4JI0jStI77vc6o0HTasi-bQ6fqPu0jXSTrUaGwrfj5j8HNu0PGVDsGxB1iZzTaIhLv1A_wzWPIuzA_I4E--_3AIEICIR8jdBzOH66GrutN2B-UYDqDmCqc7ZK4OxtDrzP1hoRmH9ui2sKJbvBf_YOA6cz9TdzT6mcnI_YOuyfynMCtZbVireOleGoQCVgAAAAEvo2aeAA")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "AsmSupport")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "AsmSafone")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "MyVideoPlayer")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-REPLY_MESSAGE = getenv("REPLY_MESSAGE", "")
+REPLY_MESSAGE = getenv("REPLY_MESSAGE", "DESTROYER")
 if not REPLY_MESSAGE:
     REPLY_MESSAGE = None
 else:
